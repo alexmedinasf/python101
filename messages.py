4d7c0136fafa77b98aec60b0ba63c67b6bdfa12f
alexmedinasf <alexsales001@hotmail.com>
2024-08-20 08:42:59 -0300

    created hello and bye messages

---
+++ messages.py
@@ -0,0 +1,7 @@
+def Hello():
+    print("Hello, welcome to the rock paper scissors game!")
+    print("Choose carefully, as the computer is a formidable opponent.")
+    
+def Bye():
+    print("Thanks for playing! Goodbye.")
+    print("Hope to see you again soon!")    
